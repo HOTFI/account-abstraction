19cac6c80a2ed8d1fe1c0d919fd5ee11af299f9f
evan <evan@hotfi.com>
2024-02-04 15:58:41 +0800

    Assert final cost lt max gas limit.

--- contracts/initializable/ChainlinkPaymaster.sol
+++ contracts/initializable/ChainlinkPaymaster.sol
@@ -25,18 +25,14 @@ contract ChainlinkPaymaster is BasePaymaster {
 
     using UserOperationLib for UserOperation;
 
-    event OracleUpdated(address indexed oracle);
-
     //calculated cost of the postOp
-    uint256 constant public COST_OF_POST = 20000;
+    uint256 constant public COST_OF_POST = 15000;
 
     address public immutable theFactory;
 
     IERC20 private immutable feeToken;
 
     AggregatorV2V3Interface private immutable _tokenToEthFeed;
-
-    uint256 private lastPrice;
 
     constructor(address accountFactory, address _entryPoint, address _feeToken, address tokenToEthFeed) BasePaymaster(IEntryPoint(_entryPoint)) {
         theFactory = accountFactory;
@@ -45,10 +41,10 @@ contract ChainlinkPaymaster is BasePaymaster {
         unchecked {
             feeToken.approve(msg.sender, uint256(0) - 1);
         }
-        lastPrice =  uint256(_tokenToEthFeed.latestAnswer());
     }
 
     function getTokenValueOfEth(uint256 ethOutput) internal view returns (uint256 tokenInput){
+        uint lastPrice = uint256(_tokenToEthFeed.latestAnswer());
         uint _tokenDecimal = 10** IERC20Metadata(address(feeToken)).decimals();
         // input = output* decimals/price
         tokenInput = ethOutput * _tokenDecimal / lastPrice;
@@ -60,22 +56,22 @@ contract ChainlinkPaymaster is BasePaymaster {
       * verify the sender has enough tokens.
       * (since the paymaster is also the token, there is no notion of "approval")
       */
-    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 /*userOpHash*/, uint256 requiredPreFund)
+    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 /*userOpHash*/, uint256 maxCost)
     internal view override returns (bytes memory context, uint256 validationData) {
-        uint256 tokenPrefund = getTokenValueOfEth(requiredPreFund);
+        uint256 tokenPrefund = getTokenValueOfEth(maxCost);
 
-        require(userOp.verificationGasLimit > COST_OF_POST, "TokenPaymaster: gas too low for postOp");
+        require(userOp.verificationGasLimit > COST_OF_POST, "ChainlinkPaymaster: gas too low for postOp");
 
         if (userOp.initCode.length != 0) {
             _validateConstructor(userOp);
-            require(feeToken.balanceOf(userOp.sender) >= tokenPrefund, "TokenPaymaster: no balance (pre-create)");
+            require(feeToken.balanceOf(userOp.sender) >= tokenPrefund, "ChainlinkPaymaster: no balance (pre-create)");
         } else {
-            require(feeToken.balanceOf(userOp.sender) >= tokenPrefund, "TokenPaymaster: no balance");
+            require(feeToken.balanceOf(userOp.sender) >= tokenPrefund, "ChainlinkPaymaster: no balance");
         }
 
-        require(feeToken.allowance(userOp.sender, address(this)) >= tokenPrefund, "TokenPaymaster: no allowance");
+        require(feeToken.allowance(userOp.sender, address(this)) >= tokenPrefund, "ChainlinkPaymaster: no allowance");
         uint256 gasPriceUserOp = userOp.gasPrice();
-        return (abi.encode(userOp.sender, gasPriceUserOp), 0);
+        return (abi.encode(userOp.sender, gasPriceUserOp, maxCost, tokenPrefund), 0);
     }
     
 
@@ -91,7 +87,7 @@ contract ChainlinkPaymaster is BasePaymaster {
     // we trust our factory (and that it doesn't have any other public methods)
     function _validateConstructor(UserOperation calldata userOp) internal virtual view {
         address factory = address(bytes20(userOp.initCode[0 : 20]));
-        require(factory == theFactory, "TokenPaymaster: wrong account factory");
+        require(factory == theFactory, "ChainlinkPaymaster: wrong account factory");
     }
 
     /**
@@ -104,9 +100,13 @@ contract ChainlinkPaymaster is BasePaymaster {
     function _postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) internal override {
         //we don't really care about the mode, we just pay the gas with the user's tokens.
         (mode);
-        lastPrice =  uint256(_tokenToEthFeed.latestAnswer());
-        (address sender, uint256 gasPricePostOp) = abi.decode(context, (address, uint256));
+
+        (address sender, uint256 gasPricePostOp, /*uint256 maxCost*/,uint256 tokenPrefund) = 
+            abi.decode(context, (address, uint256, uint256, uint256));
         uint256 charge = getTokenValueOfEth(actualGasCost + COST_OF_POST * gasPricePostOp);
+        
+        require(charge <= tokenPrefund, "ChainlinkPaymaster: gas price too low");
+
         //actualGasCost is known to be no larger than the above requiredPreFund, so the transfer should succeed.
         feeToken.transferFrom(sender, address(this), charge);
     }
